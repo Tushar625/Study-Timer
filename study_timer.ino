1c17da4083a46fb35c8fa8a8a4f571275097f4ad
Tushar Das <td9940433@gmail.com>
2025-01-11 01:12:34 +0530

    added ping sound on mode change

--- study_timer.ino
+++ study_timer.ino
@@ -2,14 +2,16 @@
 #define G_PIN 5
 #define B_PIN 6
 
-#define IN_PIN 19 // digital input with pull down resistor
+#define BUZZ_PIN 19 // buzzer output
+
+#define IN_PIN 18 // digital input
 
 #define WORK_COLOR {255 / 5, 100 / 5, 0} /*dim orange*/
 #define REST_COLOR {0, 255, 0} /*green*/
 #define EXTRA_REST_COLOR {255, 0, 0} /*red*/
 
-#define WORK_DURATION 20*60*1000lu
-#define REST_DURATION 5*60*1000lu
+#define WORK_DURATION /*10*1000*/ 20*60*1000lu
+#define REST_DURATION /*10*1000*/ 5*60*1000lu
 #define BLINK_DURATION 500lu
 
 enum STATES{WORK, REST, EXTRA_REST, STATE_COUNT};
@@ -23,7 +25,7 @@ STATES current_state;
 
 unsigned long t0;
 
-bool blink_state;
+bool blink;
 
 // setting the colour of rgb led
 
@@ -36,19 +38,15 @@ void set_color(rgb color)
     analogWrite(B_PIN, color.b);
 }
 
-// if input is 1 wait for some time and return latest deigitalRead()
-// this function returns the voltage level at input pin 1 -> high, 0 -> low
+// just generate a little sound with buzzer
 
-bool debounced_input()
+void ping()
 {
-    if(digitalRead(IN_PIN))
-    {
-        delay(5); // debounce delay
+    digitalWrite(BUZZ_PIN, HIGH);
 
-        return digitalRead(IN_PIN);
-    }
+    delay(100);
 
-    return false;
+    digitalWrite(BUZZ_PIN, LOW);
 }
 
 void setup()
@@ -59,7 +57,7 @@ void setup()
 
     t0 = millis();
 
-    blink_state = true;
+    blink = true;
 
     pinMode(R_PIN, OUTPUT);
 
@@ -67,7 +65,11 @@ void setup()
 
     pinMode(B_PIN, OUTPUT);
 
-    pinMode(IN_PIN, INPUT);
+    pinMode(BUZZ_PIN, OUTPUT);
+
+    pinMode(IN_PIN, INPUT_PULLUP);
+
+    ping();
 }
 
 void loop()
@@ -85,6 +87,8 @@ void loop()
           if(millis() - t0 > WORK_DURATION)
           {
               // time for rest
+
+              ping();
               
               current_state = REST;
 
@@ -103,14 +107,18 @@ void loop()
           {
               // resting time is over
 
+              ping();
+
               current_state = EXTRA_REST;
 
               t0 = millis();  // initialize t0 for next state
           }
-          else if(debounced_input())
+          else if(digitalRead(IN_PIN) == LOW)
           {
               // our push button is pressed
               // skip rest go to work
+
+              ping();
 
               current_state = WORK;
 
@@ -127,26 +135,32 @@ void loop()
 
           if(millis() - t0 > BLINK_DURATION)
           {
-              blink_state = !blink_state;
+              blink = !blink;
 
               t0 = millis();
           }
 
           // turn the light on or off depending on blink_state
 
-          if(blink_state)
+          if(blink)
           {
               set_color(EXTRA_REST_COLOR);
+
+              //digitalWrite(BUZZ_PIN, HIGH);
           }
           else
           {
               set_color({0, 0, 0});
+
+              //digitalWrite(BUZZ_PIN, LOW);
           }
 
-          if(debounced_input())
+          if(digitalRead(IN_PIN) == LOW)
           {
               // our push button is pressed
               // user wants to start working
+
+              ping();
 
               current_state = WORK;
 
